d4f384241de3da118032babfbd33fcbed6742ab9
VINITHA M <VINITHA M@LAPTOP-0NHV2B0I>
2023-12-15 20:30:36 +0530

    Add project files.

---
+++ Features/BritishAirwaysHomePage.feature
@@ -0,0 +1,17 @@
+﻿Feature: British Airways Website Navigation
+
+Validates different navigation in British Airways Home page
+
+@BritishAirwaysNavigations
+  Scenario: Verify navigation to the British Airways Website
+    Given the user is on the Browser
+    When the user navigates to URL "https://www.britishairways.com/travel/home/public/en_gb/"
+    Then the user should be on the home page and title should be "British Airways | Book Flights, Holidays, City Breaks & Check In Online"
+
+
+@BritishAirwaysNavigations
+  Scenario: Verify navigation to the flights page
+    Given the user is on the British Airways website
+    When the user clicks on the "Flights" link
+    Then the user should be on the flights page
+
---
+++ POM/BritishAirlinesPageObject.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BritishAirlines_SpecFlowAutomationFramework.POM
+{
+    public class BritishAirlinesPageObject
+    {
+
+        //The Selenium web driver to automate the browser
+        private readonly IWebDriver _webDriver;
+
+        public BritishAirlinesPageObject(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        //Finding elements by ID
+        private IWebElement FirstNumberElement => _webDriver.FindElement(By.Id("first-number"));
+
+        public void Initiate()
+        {
+            //_webDriver = new ChromeDriver();
+        }
+
+        public void Navigate(string url)
+        {
+            _webDriver.Navigate().GoToUrl(url);
+        }
+
+        public void AssertTitle(string title)
+        {
+            string pageTitle = _webDriver.Title;
+            pageTitle.Should().Be(title);
+        }
+    }
+}
